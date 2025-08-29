5eb4449b1cfbbdf6392ce41560d56aa68adbeb23
Emad Ali Khan <142682966+EmadAliKhan@users.noreply.github.com>
2025-08-29 18:47:41 +0500

    working

--- src/app/unauthorized/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Unauthorized() {
-  return (
-    <div className="text-center py-20">
-      <h1 className="text-3xl font-bold text-red-600">Access Denied</h1>
-      <p>You do not have permission to view this page.</p>
-    </div>
-  );
-}
